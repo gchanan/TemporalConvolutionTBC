d4c8516d4093b282ac985b8091b19bb9e11dd893
Gregory Chanan <gchanan@fb.com>
2017-04-18 14:59:41 -0700

    Generate gpu with generic type.

--- lib/generic/TemporalConvolutionTBCHost.cpp
+++ lib/generic/TemporalConvolutionTBCHost.cpp
@@ -1,14 +1,8 @@
 // Copyright 2016 Facebook
 
-#include "THC.h"
-#include "THCTensor.h"
-#include "THCDeviceTensor.cuh"
-#include "THCDeviceTensorUtils.cuh"
-#include "THCDeviceUtils.cuh"
-#include "TemporalConvolutionTBC.cuh"
-
-#include <cuda_runtime.h>
-
+#ifndef THC_GENERIC_FILE
+#define THC_GENERIC_FILE "generic/TemporalConvolutionTBCHost.cpp"
+#else
 
 using namespace std;
 
@@ -16,17 +10,17 @@ namespace {
 
 extern "C" {
 
-int TemporalConvolutionTBC_updateOutput_Cuda(THCState *state,
-                                             THCudaTensor *output,
-                                             THCudaTensor *input,
-                                             THCudaTensor *weight,
-                                             THCudaTensor *bias) {
-  THAssert(THCudaTensor_checkGPU(state, 4, input, output, weight, bias));
+int TemporalConvolutionTBC_(updateOutput)(THCState *state,
+                                          THCTensor *output,
+                                          THCTensor *input,
+                                          THCTensor *weight,
+                                          THCTensor *bias) {
+  THAssert(THCTensor_(checkGPU)(state, 4, input, output, weight, bias));
 
-  auto inputDev = toDeviceTensor<float, 3>(state, input);
-  auto outputDev = toDeviceTensor<float, 3>(state, output);
-  auto weightDev = toDeviceTensor<float, 3>(state, weight);
-  auto biasDev = toDeviceTensor<float, 1>(state, bias);
+  auto inputDev = toDeviceTensor<real, 3>(state, input);
+  auto outputDev = toDeviceTensor<real, 3>(state, output);
+  auto weightDev = toDeviceTensor<real, 3>(state, weight);
+  auto biasDev = toDeviceTensor<real, 1>(state, bias);
 
   detail::runTemporalConvolutionTBC_updateOutput(
       state, inputDev, outputDev, weightDev, biasDev);
@@ -34,15 +28,15 @@ int TemporalConvolutionTBC_updateOutput_Cuda(THCState *state,
   return 0;
 }
 
-int TemporalConvolutionTBC_updateGradInput_Cuda(THCState *state,
-                                                THCudaTensor *dOutput,
-                                                THCudaTensor *dInput,
-                                                THCudaTensor *weight) {
-  THAssert(THCudaTensor_checkGPU(state, 3, dInput, dOutput, weight));
+int TemporalConvolutionTBC_(updateGradInput)(THCState *state,
+                                             THCTensor *dOutput,
+                                             THCTensor *dInput,
+                                             THCTensor *weight) {
+  THAssert(THCTensor_(checkGPU)(state, 3, dInput, dOutput, weight));
 
-  auto dInputDev = toDeviceTensor<float, 3>(state, dInput);
-  auto dOutputDev = toDeviceTensor<float, 3>(state, dOutput);
-  auto weightDev = toDeviceTensor<float, 3>(state, weight);
+  auto dInputDev = toDeviceTensor<real, 3>(state, dInput);
+  auto dOutputDev = toDeviceTensor<real, 3>(state, dOutput);
+  auto weightDev = toDeviceTensor<real, 3>(state, weight);
 
   detail::runTemporalConvolutionTBC_updateGradInput(
       state, dInputDev, dOutputDev, weightDev);
@@ -50,18 +44,18 @@ int TemporalConvolutionTBC_updateGradInput_Cuda(THCState *state,
   return 0;
 }
 
-int TemporalConvolutionTBC_accGradParameters_Cuda(THCState *state,
-                                                  THCudaTensor *input,
-                                                  THCudaTensor *dOutput,
-                                                  THCudaTensor *dWeight,
-                                                  THCudaTensor *dBias,
-                                                  float scale) {
-  THAssert(THCudaTensor_checkGPU(state, 4, input, dOutput, dWeight, dBias));
+int TemporalConvolutionTBC_(accGradParameters)(THCState *state,
+                                               THCTensor *input,
+                                               THCTensor *dOutput,
+                                               THCTensor *dWeight,
+                                               THCTensor *dBias,
+                                               accreal scale) {
+  THAssert(THCTensor_(checkGPU)(state, 4, input, dOutput, dWeight, dBias));
 
-  auto inputDev = toDeviceTensor<float, 3>(state, input);
-  auto dOutputDev = toDeviceTensor<float, 3>(state, dOutput);
-  auto dWeightDev = toDeviceTensor<float, 3>(state, dWeight);
-  auto dBiasDev = toDeviceTensor<float, 1>(state, dBias);
+  auto inputDev = toDeviceTensor<real, 3>(state, input);
+  auto dOutputDev = toDeviceTensor<real, 3>(state, dOutput);
+  auto dWeightDev = toDeviceTensor<real, 3>(state, dWeight);
+  auto dBiasDev = toDeviceTensor<real, 1>(state, dBias);
 
   detail::runTemporalConvolutionTBC_accGradParameters(
       state, inputDev, dOutputDev, dWeightDev, dBiasDev, scale);
@@ -71,3 +65,5 @@ int TemporalConvolutionTBC_accGradParameters_Cuda(THCState *state,
 
 }
 } // namespace
+
+#endif
--- lib/init_cuda.cpp
+++ lib/init_cuda.cpp
@@ -1,10 +1,18 @@
 #include <THCGeneral.h>
 
 #include <algorithm>
+#include <cuda_runtime.h>
+#include "THCDeviceTensor.cuh"
+#include "THCDeviceTensorUtils.cuh"
+#include "THCDeviceUtils.cuh"
+#include "TemporalConvolutionTBC.cuh"
 
-//#define TemporalConvolutionTBC_CONCAT_EXPAND(x,y,z) x ## y ## _ ## z
-//#define TemporalConvolutionTBC_CONCAT(x,y,z) TemporalConvolutionTBC_CONCAT_EXPAND(x,y,z)
-//#define TemporalConvolutionTBC_(NAME) TemporalConvolutionTBC_CONCAT(TemporalConvolutionTBC_, NAME, Real)
+#define TemporalConvolutionTBC_CONCAT_EXPAND(x,y,z) x ## y ## _ ## z
+#define TemporalConvolutionTBC_CONCAT(x,y,z) TemporalConvolutionTBC_CONCAT_EXPAND(x,y,z)
+#define TemporalConvolutionTBC_(NAME) TemporalConvolutionTBC_CONCAT(TemporalConvolutionTBC_, NAME, CReal)
 
 #include "generic/TemporalConvolutionTBCHost.cpp"
-//#include "THCGenerateFloatTypes.h"
+#include "THCGenerateFloatType.h"
+
+//#include "TemporalConvolutionTBCHost.cpp"
+//#include "THCGenerateDoubleType.h"
